7bdff608ac4c5b18273449882b716e22be41bfb2
lesley.tomosada <lesley.tomosada@cvent.com>
2024-03-20 17:03:27 -0700

    carousel working

--- crave-app/src/components/Result.tsx
+++ crave-app/src/components/Result.tsx
@@ -1,34 +1,43 @@
 import { useState } from "react";
 
 interface ResultProps {
-    list: Restaurant[];
+  list: Restaurant[];
 }
 
 export interface Restaurant {
-    id: string,
-    name: string,
-    location: {
-        address1: string;
-    };
-    distance: number;
-    url: string;
-    }
-
+  id: string;
+  name: string;
+  location: {
+    address1: string;
+  };
+  distance: number;
+  url: string;
+}
 
 const Result: React.FC<ResultProps> = ({ list }) => {
-    const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const [currentIndex, setCurrentIndex] = useState<number>(0);
+  const handleNext = () => {
+    setCurrentIndex(prevIndex => prevIndex + 1);
+  }
 
-    return (
-        <div className="result">
-        <p className="resultTitle">Result</p>       
-                <tr key={list[currentIndex].id}>
-                    <td>{list[currentIndex].name}</td>
-                    <td>{list[currentIndex].location.address1}</td>
-                    <td>{list[currentIndex].distance.toFixed(1)}m away</td>
-                    <td><a href={list[currentIndex].url}>{list[currentIndex].name}</a></td>
-                </tr>
-                </div>
- )
+    const handlePrev = () => {
+    setCurrentIndex(prevIndex => prevIndex - 1);
     }
+  return (
+    <div className="result">
+      <p className="resultTitle">Result</p>
+      <tr key={list[currentIndex].id}>
+        <p>{list[currentIndex].name}</p>
+        <p>{list[currentIndex].location.address1}</p>
+        <p>{list[currentIndex].distance.toFixed(1)}m away</p>
+        <p>
+          <a href={list[currentIndex].url}>{list[currentIndex].name}</a>
+        </p>
+      </tr>
+      <button onClick={handlePrev} disabled={currentIndex === 0}>Previous</button>
+            <button onClick={handleNext} disabled={currentIndex === list.length - 1}>Next</button>
+    </div>
+  );
+};
 
 export default Result;
